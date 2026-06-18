1bbd9f0c0c72a3b4be94d11603f87aa2e8c7def8
jwolf-12 <prachijain12026@gmail.com>
2026-06-18 22:37:27 +0530

    initial structure

---
+++ position.hpp
@@ -0,0 +1,17 @@
+#ifndef position.hpp
+#define position.hpp
+
+#include <bits/stdc++.h>
+using namespace std;
+
+namespace Connect4{
+
+class Position{
+    static const int WIDTH=7;
+    static const int HEIGHT=6;
+
+};
+
+}
+
+#endif
