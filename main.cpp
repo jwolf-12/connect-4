b1ecffc798e06499dc951aa82a2f5f7db513c0c9
jwolf-12 <prachijain12026@gmail.com>
2026-06-20 11:13:41 +0530

    testing-1

--- main.cpp
+++ main.cpp
@@ -3,27 +3,29 @@ using namespace std;
 
 #include "position.hpp"
 #include "solver.hpp"
+#include <fstream>
+
+int sign(int n){
+    if(n>0) return 1;
+    if(n==0) return 0;
+    return -1;
+}
 
 int main() {
-    Position P;
+    ifstream fin("Test_L3_R1");
 
-    string moves = "2252576253462244111563365343671351441";
+    int c=0;
 
-    for(char c : moves) {
-        int col = c - '1';
-        P.play(col);
-        cout << "played" << endl;
+    string moves;
+    int expected_score;
+    Solver s;
+    while(fin >> moves >> expected_score){
+        Position P;
+        for(auto p :  moves){
+            P.play(p-'1');
+        }
+        int score = s.negmax(P,-22,22);
+        if(sign(score)==sign(expected_score)) c+=1;
     }
-
-    for(int c=0;c<7;c++){
-        cout << c << " "
-            << P.can_play(c) << " "
-            << P.is_winning_move(c)
-            << endl;
-    }
-    P.print();
-
-    Solver S;
-    cout << P.nb_moves() << endl;
-    cout << S.negmax(P,-1,1) << endl;
+    cout << c/10.0 << endl;
 }
